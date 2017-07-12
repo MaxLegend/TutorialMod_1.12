2ebb2e4ba24e2de723f5a07970c13f9fe8a0c358
MaxLegend <maxisst58@gmail.com>
2017-07-12 17:25:46 +0400

    Update GlassCutter.java

--- src/main/java/com/tutmod/tutmod/basic/GlassCutter.java
+++ src/main/java/com/tutmod/tutmod/basic/GlassCutter.java
@@ -13,12 +13,12 @@ import net.minecraft.item.ItemTool;
 import com.google.common.collect.Sets;
 import com.tutmod.tutmod.MainClass;
 
-public class GlassCutter extends ItemTool{
+public class GlassCutter extends ItemTool {
 	  private static final Set<Block> EFFECTIVE_ON = Sets.newHashSet(Blocks.GLASS,Blocks.GLASS_PANE,Blocks.GLOWSTONE);
 	  private static final float[] ATTACK_DAMAGES = new float[] {6.0F, 8.0F, 8.0F, 8.0F, 6.0F};
 	  private static final float[] ATTACK_SPEEDS = new float[] { -3.2F, -3.2F, -3.1F, -3.0F, -3.0F};
 	    
-	  	public GlassCutter(String name, Item.ToolMaterial material, int maxDamage){
+	  	public GlassCutter(String name, Item.ToolMaterial material, int maxDamage) {
 	        super(material, EFFECTIVE_ON);
 	        this.maxStackSize = 1;
 	        this.setMaxDamage(maxDamage);
@@ -28,12 +28,12 @@ public class GlassCutter extends ItemTool{
 	        this.damageVsEntity = ATTACK_DAMAGES[material.ordinal()];
 	        this.attackSpeed = ATTACK_SPEEDS[material.ordinal()];
 	  	}
-	    	public GlassCutter(Item.ToolMaterial material, float damage, float speed){
+	    	public GlassCutter(Item.ToolMaterial material, float damage, float speed) {
 	        super(material, EFFECTIVE_ON);
 	        this.damageVsEntity = damage;
 	        this.attackSpeed = speed;
 	    	}
-	    	public float getStrVsBlock(ItemStack stack, IBlockState state){
+	    	public float getStrVsBlock(ItemStack stack, IBlockState state) {
 	        Material material = state.getMaterial();
 	        return material != Material.GLASS ? super.getStrVsBlock(stack, state) : this.efficiencyOnProperMaterial;
 	    	}

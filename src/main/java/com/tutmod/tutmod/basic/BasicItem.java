cd3f921150aa0f88d5c3dca42612a7d43000e3a6
MaxLegend <maxisst58@gmail.com>
2017-07-12 17:25:16 +0400

    Update BasicItem.java

--- src/main/java/com/tutmod/tutmod/basic/BasicItem.java
+++ src/main/java/com/tutmod/tutmod/basic/BasicItem.java
@@ -4,8 +4,8 @@ import com.tutmod.tutmod.MainClass;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 
-public class BasicItem extends Item{
-	public BasicItem(String name,int maxStackSize){
+public class BasicItem extends Item {
+	public BasicItem(String name,int maxStackSize) {
 		this.setRegistryName(name);
 		this.setCreativeTab(MainClass.tabMain);
 		this.setUnlocalizedName(name);

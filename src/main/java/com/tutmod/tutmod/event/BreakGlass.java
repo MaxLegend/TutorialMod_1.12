2375a60c6e59e1ae50b679a4b5baf04b212a361e
MaxLegend <maxisst58@gmail.com>
2017-07-12 17:23:23 +0400

    Update BreakGlass.java

--- src/main/java/com/tutmod/tutmod/event/BreakGlass.java
+++ src/main/java/com/tutmod/tutmod/event/BreakGlass.java
@@ -11,19 +11,19 @@ import com.tutmod.tutmod.items.ItemReg;
 
 public class BreakGlass {
 	@SubscribeEvent
-	public void onDrop(BlockEvent.HarvestDropsEvent event){		
+	public void onDrop(BlockEvent.HarvestDropsEvent event) {		
 	  EntityPlayer player = (EntityPlayer)event.getHarvester();
-	  if(!event.getWorld().isRemote){ 	
-			if(player != null){
+	  if(!event.getWorld().isRemote) { 	
+			if(player != null) {
 			ItemStack is = player.getHeldItem(EnumHand.MAIN_HAND);
-				if (is != null && is.getItem() == ItemReg.tuttool){
-					if(event.getState().getBlock().equals(Blocks.GLASS) ){
+				if (is != null && is.getItem() == ItemReg.tuttool) {
+					if(event.getState().getBlock().equals(Blocks.GLASS) ) {
 					event.getDrops().add(new ItemStack(Item.getItemFromBlock(Blocks.GLASS)));
 					}
-					if(event.getState().getBlock().equals(Blocks.GLASS_PANE)){
+					if(event.getState().getBlock().equals(Blocks.GLASS_PANE)) {
 					event.getDrops().add(new ItemStack(Item.getItemFromBlock(Blocks.GLASS_PANE)));	
 					}
-					if(event.getState().getBlock().equals(Blocks.GLOWSTONE)){
+					if(event.getState().getBlock().equals(Blocks.GLOWSTONE)) {
 					event.getDrops().clear();
 					event.getDrops().add(new ItemStack(Item.getItemFromBlock(Blocks.GLOWSTONE)));
 					}

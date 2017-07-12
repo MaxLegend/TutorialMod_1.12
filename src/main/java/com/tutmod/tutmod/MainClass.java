089671adaee6a3a79e2afc95432365896fd7dd02
MaxLegend <maxisst58@gmail.com>
2017-07-12 16:53:28 +0400

    Update MainClass.java

--- src/main/java/com/tutmod/tutmod/MainClass.java
+++ src/main/java/com/tutmod/tutmod/MainClass.java
@@ -7,33 +7,28 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-
 import com.tutmod.tutmod.creativetabs.TabMain;
 import com.tutmod.tutmod.proxy.CommonProxy;
 
-@Mod(modid = MainClass.MODID, name = "TutorialMod",version = MainClass.VERSION)
-public class MainClass
-{
+@Mod(modid = MainClass.MODID, name = "TutorialMod",version = MainClass.VERSION
+public class MainClass{
 	public static final String MODID = "tutmod";
 	public static final String VERSION = "0.1";
-    @SidedProxy(clientSide = "com.tutmod.tutmod.proxy.ClientProxy", serverSide = "com.tutmod.tutmod.proxy.CommonProxy")
-    public static CommonProxy proxy;
-    public static CreativeTabs tabMain = new TabMain("tabMain");
+    	public static CommonProxy proxy;
+    	public static CreativeTabs tabMain = new TabMain("tabMain");
+@SidedProxy(clientSide = "com.tutmod.tutmod.proxy.ClientProxy", serverSide = "com.tutmod.tutmod.proxy.CommonProxy")
 
     @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
+    public void preInit(FMLPreInitializationEvent event){
     	RegListEvent.init();
         proxy.preInit(event);
     }
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
+    public void init(FMLInitializationEvent event){
         proxy.init(event);
     }
     @EventHandler
-    public void postInit(FMLPostInitializationEvent event) 
-    {
+    public void postInit(FMLPostInitializationEvent event){
         proxy.postInit(event);
     }
 }

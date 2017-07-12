e3ba05d0c3c8d1ce0d9547beb2a08c72f01e5644
MaxLegend <maxisst58@gmail.com>
2017-07-12 17:22:55 +0400

    Update ItemReg.java

--- src/main/java/com/tutmod/tutmod/items/ItemReg.java
+++ src/main/java/com/tutmod/tutmod/items/ItemReg.java
@@ -10,25 +10,25 @@ import com.tutmod.tutmod.basic.BasicFood;
 import com.tutmod.tutmod.basic.BasicItem;
 import com.tutmod.tutmod.basic.GlassCutter;
 
-public class ItemReg{
+public class ItemReg {
     public static Item tutitem = new BasicItem("tutitem",64);
     public static Item tutfood = new BasicFood(20, 4F, false, "tutfood", Items.BONE);
     public static Item tuttool = new GlassCutter("tuttool",ToolMaterial.DIAMOND, 20);
 
-    public static void register(){
+    public static void register() {
         setRegister(tutfood);
         setRegister(tutitem);
         setRegister(tuttool);
     }
-    public static void registerRender(){
+    public static void registerRender() {
         setRender(tutfood);
         setRender(tutitem);
         setRender(tuttool);
     }
-    private static void registerItems(Item item){
+    private static void registerItems(Item item) {
         ForgeRegistries.ITEMS.register(item);
     }
-    private static void registerItemsRender(Item item){
+    private static void registerItemsRender(Item item) {
     	Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(item, 0, new ModelResourceLocation(item.getRegistryName(), "inventory"));
     }
 }

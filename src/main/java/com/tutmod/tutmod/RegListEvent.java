d27683dd21d616316addc30939464c689d21ff2c
MaxLegend <maxisst58@gmail.com>
2017-07-12 17:22:33 +0400

    Update RegListEvent.java

--- src/main/java/com/tutmod/tutmod/RegListEvent.java
+++ src/main/java/com/tutmod/tutmod/RegListEvent.java
@@ -5,7 +5,7 @@ import net.minecraftforge.common.MinecraftForge;
 import com.tutmod.tutmod.event.BreakGlass;
 
 public class RegListEvent {
-	public static void init(){
+	public static void init() {
 		MinecraftForge.EVENT_BUS.register(new BreakGlass());
 	}
 }

954e457b0471c1beb04d299413546307977de59c
MaxLegend <maxisst58@gmail.com>
2017-07-12 16:56:43 +0400

    Update BreakGlass.java

--- src/main/java/com/tutmod/tutmod/event/BreakGlass.java
+++ src/main/java/com/tutmod/tutmod/event/BreakGlass.java
@@ -7,36 +7,27 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumHand;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-
 import com.tutmod.tutmod.items.ItemReg;
 
 public class BreakGlass {
 	@SubscribeEvent
-	public void onDrop(BlockEvent.HarvestDropsEvent event)
-	{		
-		EntityPlayer player = (EntityPlayer)event.getHarvester();
-		if(!event.getWorld().isRemote)
-		{ 	
+	public void onDrop(BlockEvent.HarvestDropsEvent event){		
+	  EntityPlayer player = (EntityPlayer)event.getHarvester();
+	  if(!event.getWorld().isRemote){ 	
 			if(player != null){
-				ItemStack is = player.getHeldItem(EnumHand.MAIN_HAND);
-			if (is != null && is.getItem() == ItemReg.tuttool){
-			//	 if(event.getDrops() != null)
-					if(event.getState().getBlock().equals(Blocks.GLASS) )
-				{
-				
+			ItemStack is = player.getHeldItem(EnumHand.MAIN_HAND);
+				if (is != null && is.getItem() == ItemReg.tuttool){
+					if(event.getState().getBlock().equals(Blocks.GLASS) ){
 					event.getDrops().add(new ItemStack(Item.getItemFromBlock(Blocks.GLASS)));
-				}
-				if(event.getState().getBlock().equals(Blocks.GLASS_PANE))
-				{
-					event.getDrops().add(new ItemStack(Item.getItemFromBlock(Blocks.GLASS_PANE)));
-					
-				}
-				if(event.getState().getBlock().equals(Blocks.GLOWSTONE))
-				{
+					}
+					if(event.getState().getBlock().equals(Blocks.GLASS_PANE)){
+					event.getDrops().add(new ItemStack(Item.getItemFromBlock(Blocks.GLASS_PANE)));	
+					}
+					if(event.getState().getBlock().equals(Blocks.GLOWSTONE)){
 					event.getDrops().clear();
 					event.getDrops().add(new ItemStack(Item.getItemFromBlock(Blocks.GLOWSTONE)));
+					}
 				}
-			}
 			}
 		}
 	}

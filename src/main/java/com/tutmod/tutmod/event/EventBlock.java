c9818dd309eb5382701b68e1f727684d2ae304db
MaxLegend <maxisst58@gmail.com>
2017-07-18 15:01:13 +0400

    Create EventBlock.java

---
+++ src/main/java/com/tutmod/tutmod/event/EventBlock.java
@@ -0,0 +1,16 @@
+public class EventBlock {
+	@SubscribeEvent
+	public void blockClick(RightClickBlock.RightClickBlock event) {
+		
+		ItemStack is = event.getEntityPlayer().getHeldItem(EnumHand.MAIN_HAND);
+		
+		if (is != null && (is.getItem() == Items.CARROT)){
+			if(event.getWorld().getBlockState(event.getPos()).getBlock() == Blocks.GRASS) {
+				event.getWorld().setBlockState(event.getPos(), Blocks.BEDROCK.getDefaultState());
+				event.getEntityPlayer().inventory.addItemStackToInventory(new ItemStack(Blocks.GRASS));
+
+			}
+		}
+	
+}
+}
